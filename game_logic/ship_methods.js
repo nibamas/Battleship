78c5417dc97948e2e95e9bc8cd35bbd718433f9d
judy-njeru <njerujudy87@ymail.com>
2018-03-07 14:35:11 +0000

    test for checking ships coordinates

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,4 +1,15 @@
-function checkForShip () {
+function checkForShip (player, coordinates) {
+  let shipPresent, ship;
 
+  for (let i=0; i<player.ships.length; i++) {
+    ship = player.ships[i];
+
+    shipPresent = ship.locations.filter((actualCoordinate) => {
+      return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1])
+    })[0];
+    if (!shipPresent) {
+      return false;
+    }
+  }
 }
 module.exports.checkForShip =checkForShip;
