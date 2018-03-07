50da2a86ccd652fe4608ca70cb6cdbd9f0746d33
judy-njeru <njerujudy87@ymail.com>
2018-03-07 14:44:44 +0000

    checkForShip should correctly report no ship at a given players coordinate

--- test/ship_test.js
+++ test/ship_test.js
@@ -4,13 +4,6 @@ describe('checkForShip', () => {
   let checkForShip = require('../game_logic/ship_methods').checkForShip;
 
   it('should correctly report no ship at a given players coordinate', () => {
-    player = {
-      ships:[
-        {
-          locations: [[0.0]]
-        }
-      ]
-    }
-    expect(checkForShip(player, [9,9])).to.be.false;
+
   });
 });
