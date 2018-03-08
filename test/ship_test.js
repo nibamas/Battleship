ecd816cb22cdda2f7d00d46582d7722515934380
Heiðrún Sigurðardóttir <heidrunbjort@gmail.com>
2018-03-08 11:19:03 +0000

    checkForShip should handle checking multiple ships

--- test/ship_test.js
+++ test/ship_test.js
@@ -37,4 +37,22 @@ describe('checkForShip', () => {
     expect(checkForShip(player, [0, 0])).to.be.true;
     expect(checkForShip(player, [9, 9])).to.be.false;
   });
+
+  it('should handle checking multiple ships', () => {
+   player = {
+     ships:[
+       {
+         locations: [[0, 0], [0, 1]]
+       },
+       {
+         locations: [[1, 0], [1, 1]]
+       }
+     ]
+   }
+   expect(checkForShip(player, [0, 1])).to.be.true;
+   expect(checkForShip(player, [0, 0])).to.be.true;
+   expect(checkForShip(player, [1, 0])).to.be.true;
+   expect(checkForShip(player, [1, 1])).to.be.true;
+   expect(checkForShip(player, [9, 9])).to.be.false;
+ });
 });
