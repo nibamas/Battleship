c5feeddbffd450bf06dc9af8acd22dc788709faa
judy-njeru <njerujudy87@ymail.com>
2018-03-08 10:45:29 +0000

    should run tests using npm

---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+//test suite -  block of unit tests closely related that taste the same funtion or similar parts of the code base
+describe('Mocha', () => {
+  //test spec(individual test)
+  it('should run our tests using npm', () => {
+    expect(true).to.be.ok;
+  });
+})
